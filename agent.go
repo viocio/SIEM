bc05ceca20f6fb74fdc9c23e2bc5c1db213b1ff5
Maravela Viorel <viorel.maravela29@gmail.com>
2025-03-19 21:16:51 +0200

    Scheletul scriptului agent

--- agent.go
+++ agent.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Go detecteaza pe ce sistem de operare lucreaza si salveaza tipul in variabila GOOS din runtime, tip de date:char
+	switch runtime.GOOS {
+	case "linux":
+		monitorizeazaLinux()
+	case "windows":
+		monitorizeazaWindows()
+	default:
+		log.Fatal("Sistemul de operare nu este suportat.")
+	}
+}
+
+//    #####		Implementare SCRIPT pentru Linux 		######
+
+func monitorizeazaLinux() {
+
+}
+
+//    #####		Implementare SCRIPT pentru Windows 		######
+
+func monitorizeazaWindows() {
+
 }
