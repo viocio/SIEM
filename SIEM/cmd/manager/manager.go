ccecce26a2b6865c56d17376352601a0993232eb
Maravela Viorel <viorel.maravela29@gmail.com>
2025-04-09 23:19:38 +0300

    Am adaugat niste comentarii si am stat sa inteleg bine codul

--- SIEM/cmd/manager/manager.go
+++ SIEM/cmd/manager/manager.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/alert", primesteAlerta)
+	http.HandleFunc("/alert", primesteAlerta) // ii spui managerului ce functie sa apeleze daca primeste trafic pe ruta alert
 
 	port := ":58917"
 	fmt.Println("[+] Managerul ascultă pe http://localhost" + port)
@@ -18,20 +18,20 @@ func main() {
 }
 
 func primesteAlerta(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodPost { // ce se intampla daca cererea http de la agent nu estse de tip POST
 		http.Error(w, "Doar POST este permis", http.StatusMethodNotAllowed)
 		return
 	}
 
 	var a alerta.Alerta
-	err := json.NewDecoder(r.Body).Decode(&a)
+	err := json.NewDecoder(r.Body).Decode(&a) // se decodeaza datele primite din request din JSON in formatul strcturei de date Alerta pe care am creat-o noi
 	if err != nil {
 		http.Error(w, "JSON invalid", http.StatusBadRequest)
 		log.Println("[!] Alertă primită cu JSON invalid:", err)
 		return
 	}
 
-	fmt.Println("\n================ ALERTĂ NOUĂ ================ ")
+	fmt.Println("\n================ ALERTĂ NOUĂ ================ ") // Se printeaza alerta
 	fmt.Println("🛡️  Sistem   :", a.Sistem)
 	fmt.Println("🚨 Tip       :", a.Tip)
 	fmt.Println("📝 Detalii   :", a.Descriere)
--- SIEM/pkg/api/client.go
+++ SIEM/pkg/api/client.go
@@ -9,16 +9,16 @@ import (
 	"siem/pkg/alerta"
 )
 
-func TrimiteAlerta(a alerta.Alerta) {
+func TrimiteAlerta(a alerta.Alerta) { // in Golang parametrii unei functii se scriu dupa tiparul : <nume_param> <tip_de_date>
 	url := "http://192.168.229.43:58917/alert" // endpoint-ul managerului
 
-	jsonData, err := json.Marshal(a)
+	jsonData, err := json.Marshal(a) // transformam alerta in obiect JSON
 	if err != nil {
 		log.Printf("Eroare la serializarea alertei: %v", err)
 		return
 	}
 
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData)) // se trimite o cerere catre url, care e de tipul application/json, iar ultimul argument e efectiv acel json
 	if err != nil {
 		log.Printf("Eroare la trimiterea alertei: %v", err)
 		return
